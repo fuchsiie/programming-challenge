75b56e1ab81772071a508ed9f86dd36f04391a2a
Marc Fuchs <marc.fuchs@uni-ulm.de>
2023-06-08 16:18:38 +0200

    update App.java

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,10 +1,16 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.model.Football;
+import de.exxcellent.challenge.model.Weather;
+import de.exxcellent.challenge.utils.FootballUtils;
+import de.exxcellent.challenge.utils.WeatherUtils;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * @author Marc Fuchs <marcf1108@hotmail.de>
  */
 public final class App {
 
@@ -15,11 +21,18 @@ public final class App {
     public static void main(String... args) {
 
         // Your preparation code …
+        Weather smallestTempDiffEntry = WeatherUtils.getSmallestMinMaxTempDiffDay("de/exxcellent/challenge/weather.csv");
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
+        String dayWithSmallestTempSpread = smallestTempDiffEntry != null
+            ? Integer.toString(smallestTempDiffEntry.getDay())
+            : "There was an error finding the requested day!";     // Your day analysis function call …
         System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+        Football smallestGoalSpreadEntry = FootballUtils.getSmallestGoalsDiffTeam("de/exxcellent/challenge/football.csv");
+
+        String teamWithSmallestGoalSpread = smallestGoalSpreadEntry != null
+            ? smallestGoalSpreadEntry.getTeam()
+            : "There was an error finding the requested football team"; // Your goal analysis function call …
         System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
     }
 }
