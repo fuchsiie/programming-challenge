ce9a1d091369f09d0fceddf1703b624e0acb1a3f
Marc Fuchs <marc.fuchs@uni-ulm.de>
2023-06-08 13:32:45 +0200

    add calcDeltaGoals and toString to Football model class

--- src/main/java/de/exxcellent/challenge/model/Football.java
+++ src/main/java/de/exxcellent/challenge/model/Football.java
@@ -9,4 +9,21 @@ public class Football extends Data {
     private int goals;
     private int goals_allowed;
     private int points;
+
+    public int calcDeltaGoals() {
+        return Math.abs(this.goals - this.goals_allowed);
+    }
+
+    @Override
+    public String toString() {
+        return "Football [Team=" + this.team
+        + ", Games=" + this.games
+        + ", Wins=" + this.wins
+        + ", Losses=" + this.losses
+        + ", Draws=" + this.draws
+        + ", Goals=" + this.goals
+        + ", Goals Allowed=" + this.goals_allowed
+        + ", Points=" + this.points
+        + "]";
+    }
 }
