2065c4425927d443c9dba7b0569be7838b7a73ed
Marc Fuchs <marc.fuchs@uni-ulm.de>
2023-06-08 13:15:22 +0200

    add model for Football and Weather

---
+++ src/main/java/de/exxcellent/challenge/model/Football.java
@@ -0,0 +1,12 @@
+package de.exxcellent.challenge.model;
+
+public class Football extends Data {
+    private String team;
+    private int games;
+    private int wins;
+    private int losses;
+    private int draws;
+    private int goals;
+    private int goals_allowed;
+    private int points;
+}
---
+++ src/main/java/de/exxcellent/challenge/model/Weather.java
@@ -0,0 +1,18 @@
+package de.exxcellent.challenge.model;
+
+public class Weather extends Data {
+    private int day;
+    private int maximumTemperature;
+    private int minimumTemperature;
+    private int avT;
+    private float avDP;
+    private int oneHrP_TPcpn;
+    private int pDir;
+    private float avSp;
+    private int dir;
+    private int mxS;
+    private float skyC;
+    private int mxR;
+    private int mn;
+    private float r_AvSLP;
+}
