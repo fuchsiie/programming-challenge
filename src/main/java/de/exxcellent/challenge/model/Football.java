b2ded61a264b670b53d2d2250db4408cae68ed82
Marc Fuchs <marc.fuchs@uni-ulm.de>
2023-06-08 13:40:57 +0200

    add Comments for football model

--- src/main/java/de/exxcellent/challenge/model/Football.java
+++ src/main/java/de/exxcellent/challenge/model/Football.java
@@ -1,5 +1,10 @@
 package de.exxcellent.challenge.model;
 
+/**
+ * Class represents one football team.
+ *
+ * @author Marc Fuchs <marcf1108@hotmail.de>
+ */
 public class Football extends Data {
     private String team;
     private int games;
@@ -10,10 +15,17 @@ public class Football extends Data {
     private int goals_allowed;
     private int points;
 
+    /**
+     * Calculates the absolute difference between goals and goals_allowed.
+     * @return Returns the calculated difference.
+     */
     public int calcDeltaGoals() {
         return Math.abs(this.goals - this.goals_allowed);
     }
 
+    /**
+     * Returns a String that represents the Object and its values.
+     */
     @Override
     public String toString() {
         return "Football [Team=" + this.team
