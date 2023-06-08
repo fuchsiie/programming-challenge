793abf302a110d9f9cd34dc7e95a695dd75a104f
Marc Fuchs <marc.fuchs@uni-ulm.de>
2023-06-08 13:30:01 +0200

    add calcDeltaTemperature and toString to Weather model class

--- src/main/java/de/exxcellent/challenge/model/Weather.java
+++ src/main/java/de/exxcellent/challenge/model/Weather.java
@@ -15,4 +15,28 @@ public class Weather extends Data {
     private int mxR;
     private int mn;
     private float r_AvSLP;
+
+    public int calcDeltaTemperature() {
+        return this.maximumTemperature - this.minimumTemperature;
+    }
+
+    @Override
+    public String toString() {
+        return "Weather [Day=" + this.day
+        + ", maximumTemperature=" + this.maximumTemperature
+        + ", minimumTemperature=" + this.minimumTemperature
+        + ", AvT=" + this.avT
+        + ", AvDP=" + this.avDP
+        + ", 1HrP TPcpn=" + this.oneHrP_TPcpn
+        + ", PDir=" + this.pDir
+        + ", AvSp=" + this.avSp
+        + ", Dir=" + this.dir
+        + ", MxS=" + this.mxS
+        + ", SkyC=" + this.skyC
+        + ", MxR=" + this.mxR
+        + ", Mn=" + this.mn
+        + ", R AvSLP=" + this.r_AvSLP
+        + ", calcDeltaTemperature=" + this.calcDeltaTemperature()
+        + "]";
+    }
 }
